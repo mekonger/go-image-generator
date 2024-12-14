2978db0416b2aafa4435286f4148926a3d3849f7
mekonger <m3kong.dev@gmail.com>
2024-12-14 15:38:14 +0700

    demo with routine and channel

--- internal/initialize/run.go
+++ internal/initialize/run.go
@@ -1,11 +1,45 @@
 package initialize
 
-import "github.com/mekonger/go-image-generator/config"
+import (
+	"fmt"
+	"github.com/mekonger/go-image-generator/config"
+	"math/rand"
+	"time"
+)
+
+func sleepSomeTime() string {
+	sleepTime := time.Duration(rand.Intn(60)) * time.Second
+	message := fmt.Sprintf("Sleeping for %v", sleepTime)
+	fmt.Printf("About to process: %s\n", message)
+	time.Sleep(sleepTime)
+	return message
+}
+
+func job(i int, rets chan string) {
+	sleepSomeTime()
+	rets <- fmt.Sprintf("Handle the job: %d", i+1)
+}
+
+func runTasks() {
+	taskN := 100
+	rets := make(chan string, taskN)
+
+	for i := 0; i < taskN; i++ {
+		go job(i, rets)
+	}
+
+	for i := 0; i < taskN; i++ {
+		fmt.Println("message: ", <-rets)
+		time.Sleep(20 * time.Second)
+	}
+}
 
 func Run() {
 	config.LoadConfig()
 
 	r := InitRouter()
+
+	go runTasks()
 
 	err := r.Run(":8080")
 	if err != nil {
